4d23b1ad9d93fd0571be82b664fd2909554752e6
choi <puch93@naver.com>
2020-03-14 16:22:20 +0900

    2020-03-14 commit

--- app/src/main/java/kr/co/planet/newgreatluck/server/Cvalue.java
+++ app/src/main/java/kr/co/planet/newgreatluck/server/Cvalue.java
@@ -3,6 +3,7 @@ package kr.co.planet.newgreatluck.server;
 public class Cvalue {
     // "siteUrl" 로 던지는 값
     public static final String Domain = "greatluck.alrigo.co.kr";
+//    public static final String Domain = "greatluck.alrigo.co.kr";
 
     // 실제 통신에 필요한 값
     public static final String Domain2 = "https://greatluck.alrigo.co.kr:450/";
